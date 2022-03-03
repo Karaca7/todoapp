e685cc336e1de4fb712ef1a7764879d569339354
Karaca7 <karacaylmaz2016@gmail.com>
2022-03-03 22:44:37 +0300

    Create frontend project
    Created a project with vue for the frontend called appfront and placed
    the form elements in the application input.

---
+++ appfront/src/components/HelloWorld.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="hello">
+    <!-- <form oninput="sonuc.value=deger.value">
+      <input
+        type="range"
+        name="deger"
+        min="0"
+        max="10"
+        value="2"
+        ref="todoweight"
+      />
+      <output name="sonuc">0</output>
+    </form> -->
+    <div>
+      <input type="text" name="" id="" />
+      <br />
+
+      <input type="range" min="1" max="10" step="1" v-model="value" />
+      <p style="display: inline">{{ value }}</p>
+      <br />
+
+      <select name="todo" id="">
+        <option value="1">İş</option>
+        <option value="2">Okul</option>
+        <option value="3">Aile</option>
+        <option value="4">Ödemeler</option>
+        <option value="5">Kişisel Gelişim</option>
+      </select>
+      <hr />
+      <button @click="weightShifter">add</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      hel: "-", //this.$refs.todoweight,
+      value: 0,
+    };
+  },
+  methods: {
+    weightShifter() {
+      console.log("gönderildi");
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
